3ae3c287e6dc0d7f09ac00b137c52cbfecae6da7
deboo <deboo@HUSKER>
2025-02-05 22:24:15 +0200

    Add project files.

---
+++ task_4/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Йоу шановний, введiть кiлометри: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Це " + (a / 1.609) + " сухопутних миль" + " та " + ((a / 1.852)) + " морських миль ");
